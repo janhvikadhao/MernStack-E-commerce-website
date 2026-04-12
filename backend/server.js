ae870b3a0ba19653bcd9d3a4fdf37d2bcdd94a86
Janhvi Kadhao <kadhaojanhvi@gmail.com>
2026-04-13 03:10:43 +0530

    fixed axios baseURL

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the
 app.use(cookieParser());
 
 app.use(cors({
-	origin: "https://mern-stack-e-commerce-website-vuok-cyjtd7gxy.vercel.app",
+	origin: "https://mern-stack-e-commerce-website-vuok-cyjtd7gxy.vercel.app/",
 	credentials: true
   }));
 app.get("/", (req, res) => {
