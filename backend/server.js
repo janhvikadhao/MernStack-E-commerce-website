36256cbde867a76e06c57c171797503fac962ec1
Janhvi Kadhao <kadhaojanhvi@gmail.com>
2026-04-13 02:14:48 +0530

    connect backend

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,10 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+	res.send("Backend is working 🚀");
+  });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
